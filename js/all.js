d2512e81ff66eb0fef8556372b877d1060fbf86d
YiJing0502 <w71080635@gmail.com>
2023-08-06 23:02:28 +0800

    nav + side menu

---
+++ js/all.js
@@ -0,0 +1,19 @@
+const body = document.querySelector('body'),
+      sideMenu = document.querySelector('.side-menu'),
+      toggle = document.querySelector('.toggle');
+      mdSize = window.matchMedia('(max-width: 767px)');
+      
+      toggle.addEventListener('click', (e) => {
+        if (e > mdSize) {
+            sideMenu.classList.toggle('close');
+        }
+      })
+
+    document.onclick = function(e) {
+        if(e.target.class !== 'sideMenu' && e.target.class !== 'toggle') {
+            sideMenu.classList.toggle('side-menu-none');
+        }
+    }
+    
+
+console.log(toggle);
